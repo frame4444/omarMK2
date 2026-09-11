818d1adb003c02924e5d8c549f5487614549dac1
frame <marcoanguloe69@gmail.com>
2026-09-10 19:42:46 -0600

    Omar has awaken

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod actions;
+mod handlers;
+
+use handlers::handle_text_message;
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok(); // carga el .env al entorno del proceso
+    pretty_env_logger::init();
+    log::info!("Arrancando el bot...");
+
+    let omar = Bot::from_env();
+
+    teloxide::repl(omar, handle_text_message).await;
 }
